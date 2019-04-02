42f861fe523e401a97ed6458417c360128a7edbd
JinganGuo <gja1026@163.com>
2019-04-02 23:44:34 +0800

    引入d3.js 构造一个图表demo

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,12 +2,20 @@
   <div>
     主页
     <router-link to="/user-search">查找用户</router-link>
+    <p>Vue + d3 测试</p>
+    <area-chart />
+
   </div>
 </template>
 
 <script>
+import AreaChart from '../components/visual-components/AreaChart';
+
 export default {
   name: 'HomePage',
+  components: {
+    AreaChart
+  }
 }
 </script>
 
