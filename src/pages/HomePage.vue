b06776acd6d56b0e91065e8574b6fc1879a774d5
JinganGuo <gja1026@163.com>
2019-04-03 23:15:02 +0800

    添加F2.js框架

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,17 +4,21 @@
     <router-link to="/user-search">查找用户</router-link>
     <p>Vue + d3 测试</p>
     <area-chart />
+    <f-demo />
 
   </div>
 </template>
 
 <script>
 import AreaChart from '../components/visual-components/AreaChart';
+import FDemo from '../components/visual-components/FDemo';
+
 
 export default {
   name: 'HomePage',
   components: {
-    AreaChart
+    AreaChart,
+    FDemo
   }
 }
 </script>
