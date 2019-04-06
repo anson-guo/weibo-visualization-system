a7a60346ce1eda8529239df4acc115804a51cca6
JinganGuo <gja1026@163.com>
2019-04-06 23:47:02 +0800

    主页样式

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,30 +1,52 @@
-<template>  
-  <div>
-    主页
-    <router-link to="/user-search">查找用户</router-link>
-    <p>Vue + d3 测试</p>
-    <area-chart />
-    <f-demo />
+<template>
+  <div class="home">
+    <div class="container">
+      <h1>基于D3.js的微博数据可视化系统</h1>
 
+      <div class="foot">
+        <el-button type="primary">
+          <router-link to="/user-search">查找用户</router-link>
+        </el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import AreaChart from '../components/visual-components/AreaChart';
-import FDemo from '../components/visual-components/FDemo';
-
+import AreaChart from "../components/visual-components/AreaChart";
+import FDemo from "../components/visual-components/FDemo";
 
 export default {
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     AreaChart,
     FDemo
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
+.home {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  .container {
+    width: 75%;
+    height: 50vh;
+    border:2px solid  lightblue;
+    border-radius: 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    h1 {
+      text-align: center;
+      color: #3f3f3f;
+    }
+    .foot {
+      display: flex;
+      justify-content: center;
+    }
+  }
+}
 </style>
-
-
