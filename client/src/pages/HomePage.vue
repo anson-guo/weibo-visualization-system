d937db3436acd1edad843853f13cc94ad5a51b37
JinganGuo <gja1026@163.com>
2019-04-13 23:51:54 +0800

    添加主页

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,51 +1,102 @@
 <template>
   <div class="home">
-    <div class="container">
-      <h1>基于 D3 的微博数据可视化系统</h1>
+    <video
+      src="../assets/Over-the-Map.mp4"
+      autoplay="true"
+      poster="../assets/Over-the-Map.jpg"
+      loop="true"
+    >抱歉，您的浏览器不支持内嵌视频，不过不用担心，你可以下载并用你喜欢的播放器观看!</video>
 
-      <div class="foot">
-        <el-button type="primary">
-          <router-link to="/user-search">查找用户</router-link>
-        </el-button>
-      </div>
-    </div>
+    <el-container class="container">
+      <el-header class="header">
+        <h1>基于 D3 的微博数据可视化系统</h1>
+      </el-header>
+
+      <el-main>
+        <img class="info-pic" src="../assets/home.png" alt="图标">
+        <div class="info">
+          <p>———— 一个集数据采集和数据可视化与一体的微博数据可视化平台</p>
+          <div class="info-btn">
+            <el-button type="primary">
+              <router-link to="/user-search">查找用户</router-link>
+            </el-button>
+            <el-button type="primary">
+              <router-link to>热门微博</router-link>
+            </el-button>
+          </div>
+        </div>
+      </el-main>
+
+      <el-footer class="footer">
+        <span>郭敬安 |</span> 四川师范大学
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
+  
 <script>
-import AreaChart from "../components/visual-components/AreaChart";
-import FDemo from "../components/visual-components/FDemo";
-
 export default {
   name: "HomePage",
-  components: {
-    AreaChart,
-    FDemo
-  }
+  components: {}
 };
 </script>
 
 <style lang="scss" scoped>
 .home {
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100vh;
-  .container {
-    width: 75%;
-    height: 50vh;
-    border:2px solid  lightblue;
-    border-radius: 10px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    h1 {
-      text-align: center;
-      color: #3f3f3f;
+  overflow: hidden;
+}
+
+.container {
+  width: 70%;
+  background: rgba($color: #f6f6f6, $alpha: 0.3);
+  border-radius: 15px;
+  color: #000000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  .header {
+    padding-top: 50px;
+    text-align: center;
+    font-size: 24px;
+  }
+  .info-pic {
+    width: 200px;
+    float: left;
+  }
+  .info {
+    float: left;
+    padding: 20px 30px;
+    p {
+      padding-top: 30px;
     }
-    .foot {
-      display: flex;
-      justify-content: center;
+    .info-btn {
+      margin-top: 80px;
+      text-align: center;
+      button {
+        margin: 0 30px;
+        &:hover {
+          a {
+            color: #ffffff;
+          }
+        }
+        a {
+          color: #3f3f3f;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+  .footer {
+    text-align: right;
+    span {
+      font-size: 20px;
     }
   }
 }
