eba00fef319a212bb88d3448bd8966d76893be6e
JinganGuo <gja1026@163.com>
2019-04-20 22:02:32 +0800

    主要部分头部样式

---
+++ client/src/components/common-components/MainHeader/MainHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="header-container"
+    style="background-image: url('http://img.t.sinajs.cn/t5/skin/public/covervip/2012.jpg')"
+  >
+    <div class="info">
+      <div class="info-img">
+        <img
+          src="https://tva3.sinaimg.cn/crop.0.0.640.640.180/bfc243a3jw8efzr4c9ajij20hs0hsaav.jpg"
+          alt="头像"
+        >
+      </div>
+      <h3>回忆专用小马甲</h3>
+      <p>微博人气博主</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHeader"
+};
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  height: 200px;
+  .info {
+    text-align: center;
+    color: #ffffff;
+    .info-img {
+      padding-top: 30px;
+      img {
+        width: 90px;
+        border-radius: 50%;
+      }
+    }
+    h3 {
+      margin: 8px 0;
+    }
+    p {
+      margin: 0;
+      font-size: 14px;
+    }
+  }
+}
+</style>
+
+
--- client/src/pages/UserInfo.vue
+++ client/src/pages/UserInfo.vue
@@ -1,42 +1,64 @@
 <template>
-  <div class="container">
-    <el-container class="body">
-      <el-header>
-        <img src="" alt="">
-      </el-header>
+  <div class="main">
+    <el-header class="header">
+      <main-header ></main-header>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">  
+        <el-menu>
+          <el-menu-item index="1">
+            <i class="el-icon-document"></i>
+            <span slot="title">基本信息</span>
+          </el-menu-item>
+
+          <el-menu-item index="2">
+            <i class="el-icon-message"></i>
+            <span slot="title">粉丝数据</span>
+          </el-menu-item>
+
+          <el-menu-item index="3">
+            <i class="el-icon-document"></i>
+            <span slot="title">微博数据</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
       <el-container>
-        <el-main>Main</el-main>
+        <el-main></el-main>
+        <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
+import MainHeader from "../components/common-components/MainHeader";
+
 export default {
   name: "UserInfo",
+  components: {
+    MainHeader
+  },
   data() {
     return {
 
-    }
-  },
-  mounted() {
-
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.container {
-  margin: 0;
-  padding: 0;
-  background: #b4daf0;
+.main {
   height: 100vh;
-  .body {
-    background: #ffffff;
-    width: 70%;
-    margin: 0 auto;
-    height: 80vh;
-  }
+}
+.el-header {
+  padding: 0;
+}
+.header {
+  height: 200px !important;
+}
+
+.el-aside {
+  color: #333;
 }
 </style>
 
