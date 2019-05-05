6d3984b2f351116f6cd181a761843bdf6f8777c8
JinganGuo <gja1026@163.com>
2019-05-06 00:58:08 +0800

    将时间轴从主页迁移到时间轴菜单项中

--- client/src/pages/BaseInfo.vue
+++ client/src/pages/BaseInfo.vue
@@ -3,21 +3,18 @@
     <personal-info :userData="userData" :imgUrl="imgUrl"></personal-info>
     <card-list :userCharData="userCharData"></card-list>
     <hr>
-    <time-line v-if="timeLineData.length" :timeLineData="timeLineData"></time-line>
   </div>
 </template>
 
 <script>
 import CardList from "./PageComponents/CardList";
 import PersonalInfo from "./PageComponents/PersonalInfo";
-import TimeLine from "../components/common-components/TimeLine";
 
 export default {
   name: "BaseInfo",
   components: {
     PersonalInfo,
-    CardList,
-    TimeLine
+    CardList
   },
   props: {
     userBaseInfo: Object
@@ -26,62 +23,8 @@ export default {
     return {
       userData: [], // 用户个人信息
       imgUrl: "", // 用户头像url地址
-      userCharData: {}, // 用户图表相关数据，粉丝数、关注数、微博数量、图片数量等
-      weibos: [], // 用户的微博数据
-      timeLineData: [] // 用户微博时间线数据
+      userCharData: {} // 用户图表相关数据，粉丝数、关注数、微博数量、图片数量等
     };
-  },
-  methods: {
-    /**
-     * 获取微博数据
-     */
-    fetchWeiboData(callback) {
-      const id = this.$route.path.split("/")[2];
-      const url = `/api/user-info/${id}/weibos`;
-
-      this.$axios
-        .get(url, {
-          id
-        })
-        .then(res => {
-          if (+res.status === 200) {
-            this.weibos = res.data;
-            this.formatTimeLineData();
-          }
-        });
-    },
-
-    /**
-     * 构造时间线格式的数据
-     */
-    formatTimeLineData() {
-      const sources = this.weibos.map((item, index) => {
-        return item.source;
-      });
-
-      const sourceList = new Set(sources);
-
-      // 获取第一条微博
-      this.timeLineData.push({
-        content: "微博",
-        timestamp: this.weibos[0].created_at,
-        color: "#0bbd87"
-      });
-
-      const colorList = ["#cd484d", "#ec8b7a", "#edb789", "#eee7cb", "#eee7cb"];
-
-      const _this = this;
-      this.weibos.forEach((item, index) => {
-        if (sourceList.has(item.source)) {
-          _this.timeLineData.push({
-            content: item.source,
-            timestamp: item.created_at,
-            color: colorList[index % colorList.length]
-          });
-          sourceList.delete(item.source);
-        }
-      });
-    }
   },
   watch: {
     userBaseInfo(val) {
@@ -131,9 +74,6 @@ export default {
         weiboImgs: val.weiboImgs
       };
     }
-  },
-  created() {
-    this.fetchWeiboData();
   }
 };
 </script>
--- client/src/pages/RelatedApp.vue
+++ client/src/pages/RelatedApp.vue
@@ -1,13 +1,80 @@
 <template>
-  <div>RelatedApp</div>
+  <div>
+    <time-line v-if="timeLineData.length" :timeLineData="timeLineData"></time-line>
+  </div>
 </template>
 
 <script>
+import TimeLine from "../components/common-components/TimeLine";
+
 export default {
-  name: 'RelatedApp'
-}
+  name: "RelatedApp",
+  components: {
+    TimeLine
+  },
+  data() {
+    return {
+      weibos: [], // 用户的微博数据
+      timeLineData: [] // 用户微博时间线数据
+    };
+  },
+  methods: {
+    /**
+     * 获取微博数据
+     */
+    fetchWeiboData(callback) {
+      const id = this.$route.path.split("/")[2];
+      const url = `/api/user-info/${id}/weibos`;
+
+      this.$axios
+        .get(url, {
+          id
+        })
+        .then(res => {
+          if (+res.status === 200) {
+            this.weibos = res.data;
+            this.formatTimeLineData();
+          }
+        });
+    },
+
+    /**
+     * 构造时间线格式的数据
+     */
+    formatTimeLineData() {
+      const sources = this.weibos.map((item, index) => {
+        return item.source;
+      });
+
+      const sourceList = new Set(sources);
+
+      // 获取第一条微博
+      this.timeLineData.push({
+        content: "微博",
+        timestamp: this.weibos[0].created_at,
+        color: "#0bbd87"
+      });
+
+      const colorList = ["#cd484d", "#ec8b7a", "#edb789", "#eee7cb", "#eee7cb"];
+
+      const _this = this;
+      this.weibos.forEach((item, index) => {
+        if (sourceList.has(item.source)) {
+          _this.timeLineData.push({
+            content: item.source,
+            timestamp: item.created_at,
+            color: colorList[index % colorList.length]
+          });
+          sourceList.delete(item.source);
+        }
+      });
+    }
+  },
+  created() {
+    this.fetchWeiboData();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
