ef58bc29585feff73301437f955bfcc68d8a7d21
JinganGuo <gja1026@163.com>
2019-05-08 00:01:16 +0800

    添加甜甜圈饼图组件

--- client/src/pages/FansData.vue
+++ client/src/pages/FansData.vue
@@ -5,14 +5,14 @@
         <el-card class="char-card">
           <h3>粉丝年龄分布</h3>
           <p>以饼图的形式展示该用户的粉丝年龄分布情况</p>
-          <age-piechart></age-piechart>
+          <f2-base-piechart container="fans-ages-char"></f2-base-piechart>
         </el-card>
       </el-col>
       <el-col class="col" :sm="24" :md="12" :lg="12">
         <el-card class="char-card">
           <h3>粉丝性别分布</h3>
           <p>以饼图的形式展示该用户的粉丝性别分布情况</p>
-          <sex-piechart></sex-piechart>
+          <f2-donut-char container="fans-sex-char"></f2-donut-char>
         </el-card>
       </el-col>
     </el-row>
@@ -21,7 +21,7 @@
         <el-card class="char-card">
           <h3>粉丝的粉丝数量排序</h3>
           <p>以柱状图的形式展示该用户的粉丝的粉丝，从高到低排序并取前10位</p>
-          <fans-histogram></fans-histogram>
+          <f2-base-histogram container="fans-count-histogram"></f2-base-histogram>
         </el-card>
       </el-col>
     </el-row>
@@ -29,17 +29,16 @@
 </template>
 
 <script>
-import AgePiechart from "../components/visual-components/AgePiechart";
-import SexPiechart from "../components/visual-components/SexPiechart";
-import FansHistogram from "../components/visual-components/FansHistogram";
-
+import F2BasePiechart from "../components/visual-components/F2BasePiechart";
+import F2BaseHistogram from "../components/visual-components/F2BaseHistogram";
+import F2DonutChar from "../components/visual-components/F2DonutChar";
 
 export default {
   name: "FansData",
   components: {
-    AgePiechart,
-    SexPiechart,
-    FansHistogram
+    F2BasePiechart,
+    F2BaseHistogram,
+    F2DonutChar
   },
   data() {
     return {};
