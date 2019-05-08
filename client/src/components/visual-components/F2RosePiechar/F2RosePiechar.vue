3f7483a3f7d56a17e988ec3f7ed9e9b4a67a07dd
JinganGuo <gja1026@163.com>
2019-05-08 16:19:35 +0800

    新增玫瑰饼图

---
+++ client/src/components/visual-components/F2RosePiechar/F2RosePiechar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="chart-wrapper">
+    <canvas class="rosePiechar" :id="container"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "F2RosePiechar",
+  props: {
+    container: String,
+    activityData: Array
+  },
+  methods: {
+    init() {
+      var chart = new this.$F2.Chart({
+        id: this.container,
+        pixelRatio: window.devicePixelRatio,
+        width: 400,
+        height: 400
+      });
+      chart.source(this.activityData);
+      chart.coord("polar");
+      chart.legend({
+        position: "bottom"
+      });
+      chart.axis(false);
+      chart
+        .interval()
+        .position("key*value")
+        .color("key")
+        .style({
+          lineWidth: 1,
+          stroke: "#fff"
+        });
+      chart.render();
+    }
+  },
+  watch: {
+    activityData: function() {
+      this.init();
+    }
+  },
+  mounted() {
+    this.init();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.rosePiechar {
+  width: 100%;
+  height: 50%;
+}
+</style>
+
